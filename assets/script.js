3d6fb007b4b332c97e3ec08fa2d2668e524dd2d4
Liam <liamjwatson@aol.com>
2023-01-10 15:35:01 +0000

    Validation for user password length added

--- assets/script.js
+++ assets/script.js
@@ -88,12 +88,51 @@ var upperCasedCharacters = [
   "Z",
 ];
 
+// Enter Validation requirements here
+minLength = 10;
+maxLength = 64;
+
 // Function to prompt user for password options
 function getPasswordOptions() {
+  // -------------------------VALIDATION FOR LENGTH OF PASSWORD CHECK ---------------------------
   //   Ask User how many letters they want?
   // inital number
   //  store number in var
   // store letters remaining in var
+  // -------------------------VALIDATION FOR LENGTH OF PASSWORD CHECK ----------------------------
+  function validatePasswordLength(min, max) {
+    // Prompt user for a number
+    var passwordLength = prompt(
+      "Length of password (10 - 64 characters)",
+      "10"
+    );
+    // check for not a number, under 10 or over 64
+    while (
+      typeof passwordLength !== "number" ||
+      passwordLength < min ||
+      passwordLength > max
+    ) {
+      // If number is valid - break out of while loop and continue
+      if (passwordLength >= min && passwordLength <= max) {
+        convertedAnswer = passwordLength;
+        break;
+      }
+      // if not valid, repeat prompt
+      else {
+        passwordLength = prompt(
+          "Not enough characters or too many! \n Please choose a number between 10 and 64."
+        );
+      }
+    }
+    return passwordLength;
+  }
+  // Call length of password function. Min = 10, Max = 64
+  var lengthOfPassword = validatePasswordLength(minLength, maxLength);
+  return lengthOfPassword;
+
+  // ------------------------- End VALIDATION LENGTH CHECK ----------------------------
+
+  // --------------------------------------------------------------------------------
   // prompt - confirm- user for option
   // userResponse = confirm
   // if confirm = True{
@@ -118,6 +157,12 @@ function getPasswordOptions() {
   // }
   // }
 }
+
+var validPasswordLength = getPasswordOptions();
+var letterRemaining = validPasswordLength;
+
+console.log("valid password " + validPasswordLength);
+console.log("letters remaining " + letterRemaining);
 //  get answer 5 numbers back from user
 // 1) password length
 // 2) Uppercase letters:
